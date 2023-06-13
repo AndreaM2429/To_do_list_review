8383d7a41dea2509037564291ff3ce4dc7a0a2f8
AndreaMOr <andreamanuel2207@gmail.com>
2023-06-12 18:23:31 -0600

    Apply suggested changes

--- src/Modules/complete_task.js
+++ src/Modules/complete_task.js
@@ -7,14 +7,24 @@ export const deleteComplete = (tasks) => {
   return tasks;
 };
 
-const markComplete = (tasks, index, save, load) => {
-  if (tasks[index].completed === false) {
-    tasks[index].completed = true;
-  } else {
-    tasks[index].completed = false;
+export const removeTask = (tasks, index, save, load) => {
+  tasks.splice(index, 1);
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].index = i + 1;
   }
   save();
   load();
 };
 
-export default markComplete;
+export const markComplete = (tasks, index, save, load) => {
+  tasks[index].completed = !tasks[index].completed;
+  /* if (tasks[index].completed === false) {
+    tasks[index].completed = true;
+  } else {
+    tasks[index].completed = false;
+  } */
+  save();
+  load();
+};
+
+// export default markComplete;
